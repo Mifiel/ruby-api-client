aa0587b0f1aba48ed2a707e4c6975e9a3bbf79f1
Genaro Madrid <genaro@appio.mx>
2016-01-07 14:02:40 -0600

    add sign method to document - not complete yet

--- lib/mifiel/document.rb
+++ lib/mifiel/document.rb
@@ -6,6 +6,29 @@ module Mifiel
     put :save, '/documents/:id'
     post :create, '/documents'
 
+    def sign(certificate_id, private_key:nil, signature:nil)
+      fail MifielError, 'Either private_key or signature must be provided' if !private_key && !signature
+      signature ||= sign_hash(private_key, original_hash)
+      url = "#{Mifiel::BASE_URL}/documents/#{id}/sign"
+      params = {
+        key: certificate_id,
+        signature: signature
+      }
+      Mifiel::Document._request(url, :post, params)
+    rescue ActiveRestClient::HTTPClientException => e
+      message = e.result.errors || [e.result.error]
+      raise MifielError, message.to_a.join(', ')
+    rescue ActiveRestClient::HTTPServerException
+      raise MifielError, 'Server could not process request'
+    end
+
+    private
+
+      def sign_hash(key, hash)
+        digest = OpenSSL::Digest::SHA256.new
+        ""
+      end
+
   end
 
 end
