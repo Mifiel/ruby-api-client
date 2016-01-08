38a6c3f40c09361d5c6771665600916f7b279f5f
Genaro Madrid <genaro@appio.mx>
2016-01-08 17:35:30 -0600

    modify response

--- lib/mifiel/base.rb
+++ lib/mifiel/base.rb
@@ -8,7 +8,7 @@ module Mifiel
         message = result['errors'] || [result['error']]
         raise BadRequestError, message.to_a.join(', ')
       elsif (500..599).include?(response.status)
-        raise ServerError, 'Server could not process request'
+        raise ServerError, "Server could not process your request: status #{response.status}"
       end
     end
 
