fd0fd3c73fdcafec9eb31cc0f996e7a4319953e0
Genaro Madrid <genaro@appio.mx>
2016-01-07 21:10:10 -0600

    handle errors properlu

--- lib/mifiel/base.rb
+++ lib/mifiel/base.rb
@@ -1,5 +1,16 @@
 module Mifiel
   class Base < ActiveRestClient::Base
+    after_request :rescue_errors
+
+    def rescue_errors(name, response)
+      if response.status == 400 # bad request
+        result = JSON.load(response.body)
+        message = result['errors'] || [result['error']]
+        raise BadRequestError, message.to_a.join(', ')
+      elsif (500..599).include?(response.status)
+        raise ServerError, 'Server could not process request'
+      end
+    end
 
   end
 end
--- lib/mifiel/errors.rb
+++ lib/mifiel/errors.rb
@@ -1,5 +1,7 @@
 module Mifiel
   MifielError = Class.new StandardError
+  BadRequestError = Class.new StandardError
+  ServerError = Class.new StandardError
   PrivateKeyError = Class.new StandardError
   NotPrivateKeyError = Class.new ArgumentError
 end
