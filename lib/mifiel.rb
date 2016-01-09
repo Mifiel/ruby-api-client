2ba007a6f8341ef9662a0213ec741564072c8ad0
Genaro Madrid <genaro@appio.mx>
2016-01-08 18:25:18 -0600

    autoload cert

--- lib/mifiel.rb
+++ lib/mifiel.rb
@@ -4,6 +4,7 @@ module Mifiel
   require 'mifiel/errors'
   autoload :Base, 'mifiel/base'
   autoload :Document, 'mifiel/document'
+  autoload :Certificate, 'mifiel/certificate'
   autoload :Config, 'mifiel/config'
 
   BASE_URL = 'https://www.mifiel.com/api/v1'
