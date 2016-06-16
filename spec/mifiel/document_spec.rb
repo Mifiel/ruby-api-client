02292c4bb8cb0358990ede2a683d8be2b62508cb
Genaro Madrid <genmadrid@gmail.com>
2016-06-15 21:16:53 -0500

    add server response errors specs

--- spec/mifiel/document_spec.rb
+++ spec/mifiel/document_spec.rb
@@ -21,11 +21,38 @@ describe Mifiel::Document do
 
   describe '#request_signature' do
     let!(:document) { Mifiel::Document.all.first }
+    let!(:error_body) { { errors: ['some error'] }.to_json }
 
     it '' do
       expect do
         document.request_signature('some@email.com')
       end.not_to raise_error
+    end
+
+    context 'when bad request' do
+      before do
+        url = %r{mifiel.com\/api\/v1\/documents\/#{document.id}\/request_signature}
+        stub_request(:post, url).to_return(body: error_body, status: 404)
+      end
+
+      it '' do
+        expect do
+          document.request_signature('some@email.com')
+        end.to raise_error(Mifiel::BadRequestError)
+      end
+    end
+
+    context 'when server error' do
+      before do
+        url = %r{mifiel.com\/api\/v1\/documents\/#{document.id}\/request_signature}
+        stub_request(:post, url).to_return(body: error_body, status: 500)
+      end
+
+      it '' do
+        expect do
+          document.request_signature('some@email.com')
+        end.to raise_error(Mifiel::ServerError)
+      end
     end
   end
 end
