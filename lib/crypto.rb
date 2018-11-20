322416a3e1cf8eb4903bcb8669557a2223388284
aldosolorzano <aldosolorzanop@gmail.com>
2018-11-20 13:20:56 -0600

    AES class with tests

--- lib/crypto.rb
+++ lib/crypto.rb
@@ -1,4 +1,5 @@
 module Crypto
   autoload :PBE, 'crypto/pbe'
   autoload :Response, 'crypto/response'
+  autoload :AES, 'crypto/aes'
 end
---
+++ spec/crypto/aes_spec.rb
@@ -0,0 +1,57 @@
+describe Crypto::AES do
+  aes_fixture = JSON.parse(File.read('spec/fixtures/aes.json'), symbolize_names: true)
+
+  describe '#AES good' do
+    aes_fixture[:valid].each do |v|
+      describe v.slice(:algorithm, :key, :dataToEncrypt).to_s do
+        e_args = { data: v[:dataToEncrypt], key: v[:key], iv: v[:iv], cipher: v[:algorithm] }
+        d_args = e_args.clone
+        let(:aes) { Crypto::AES.new(e_args[:cipher]) }
+        let(:encrypted) { aes.encrypt(e_args) }
+        it 'should return Encrypted instance' do
+          expect(encrypted).to be_a Crypto::Encrypted
+        end
+        it 'should return encrypted hex' do
+          expect(encrypted.to_hex).to eq(v[:encrypted])
+        end
+        it 'should receive binary data and return decipher text' do
+          d_args[:data] = encrypted.data
+          expect(aes.decrypt(d_args)).to eq(v[:dataToEncrypt])
+        end
+        it 'should receive Encrypted instance & return decipher text' do
+          d_args[:data] = encrypted
+          expect(aes.decrypt(d_args)).to eq(v[:dataToEncrypt])
+        end
+        it 'should encrypt & decrypt with static method' do
+          encrypted_data = Crypto::AES.encrypt(e_args)
+          expect(encrypted_data == encrypted).to be true
+          expect(encrypted_data.to_hex).to eq(v[:encrypted])
+          d_args[:data] = encrypted_data
+          expect(Crypto::AES.decrypt(d_args)).to eq(v[:dataToEncrypt])
+        end
+      end
+    end
+
+    describe 'Generate random iv' do
+      let(:ivs) { Set.new }
+      5.times do
+        iv = Crypto::AES.random_iv.unpack('H*').first
+        it "should be unique #{iv}" do
+          expect(ivs.include?(iv)).to be false
+          ivs.add(iv)
+        end
+      end
+    end
+  end
+
+  describe '#AES bad' do
+    describe 'ArgumentError, sending wrong data to decrypt' do
+      args = { data: 'bad-data', iv: Crypto::AES.random_iv, key: 'this-aSecure-key' }
+      let(:expected_error) { "Expected keys #{Crypto::AES.new.require_args}" }
+      it 'should raise expected error' do
+        args = {}
+        expect { Crypto::AES.decrypt(args) }.to raise_error(ArgumentError, expected_error)
+      end
+    end
+  end
+end
